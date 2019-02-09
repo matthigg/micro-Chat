b2d10ffbb16bbffea58eb0f35b4aef96a7b7d2bd
frealis <36343653+frealis@users.noreply.github.com>
2019-02-09 14:31:06 -0500

    Prevent user from submitting a blank channel name on index.html

--- static/index.js
+++ static/index.js
@@ -14,43 +14,57 @@
 // Alternatively, you could probably use Jinja2 templating to run a FOR loop over 
 // the channels[] array if you wanted to.
 
-// Custom parser that converts the "channels_string" received from the *.innerHTML
-// method into an array of channel names
-var record = false;
-var channel = '';
-var channels = [];
-var channels_string = document.getElementById("channels_hidden").innerHTML;
+document.addEventListener('DOMContentLoaded', () => {
 
-for (let i = 0; i < channels_string.length; i++) {
+  // Hide the channels string that is returned from application.py
+  document.getElementById("channels_hidden").style.visibility = "hidden";
 
-  // Start recording at the first single quote character
-  if (channels_string[i] === "'" && channel === '') {
-    record = !record;
-  // Stop recording at the second single quote character
-  } else if (channels_string[i] === "'" && channel !== '') {
-    record = !record;
-    // Use *.substring(1) to trim the first single quote character. The second
-    // single quote character is omitted by default.
-    channel = channel.substring(1)
-    // The newly trimmed "channel" string is added to the channels[] array
-    channels.push(channel);
-    channel = '';
+  // By default, submit button is disabled
+  document.querySelector('#submit').disabled = true;
+
+  // Enable button only if there is text in the input field
+  document.querySelector('#new_channel').onkeyup = () => {
+    if (document.querySelector('#new_channel').value.length > 0)
+      document.querySelector('#submit').disabled = false;
+    else
+      document.querySelector('#submit').disabled = true;
   };
-  
-  if (record) {
-    channel += channels_string[i];
+
+  // Custom parser that converts the "channels_string" received from the *.innerHTML
+  // method into an array of channel names
+  var record = false;
+  var channel = '';
+  var channels = [];
+  var channels_string = document.getElementById("channels_hidden").innerHTML;
+
+  for (let i = 0; i < channels_string.length; i++) {
+
+    // Start recording at the first single quote character
+    if (channels_string[i] === "'" && channel === '') {
+      record = !record;
+    // Stop recording at the second single quote character
+    } else if (channels_string[i] === "'" && channel !== '') {
+      record = !record;
+      // Use *.substring(1) to trim the first single quote character. The second
+      // single quote character is omitted by default.
+      channel = channel.substring(1)
+      // The newly trimmed "channel" string is added to the channels[] array
+      channels.push(channel);
+      channel = '';
+    };
+    
+    if (record) {
+      channel += channels_string[i];
+    };
   };
-};
 
-// Hide the channels string that is returned from application.py
-document.getElementById("channels_hidden").style.visibility = "hidden";
-
-// Create channel room links with names from the channels[] array
-for (let j = 0; j < channels.length; j++) {
-  var a = document.createElement('a');
-  var br = document.createElement('br');
-  a.href = 'channel/' + channels[j];
-  a.innerHTML = channels[j];
-  document.getElementById('channel_list').appendChild(a);
-  document.getElementById('channel_list').appendChild(br);
-}
+  // Create channel room links with names from the channels[] array
+  for (let j = 0; j < channels.length; j++) {
+    var a = document.createElement('a');
+    var br = document.createElement('br');
+    a.href = 'channel/' + channels[j];
+    a.innerHTML = channels[j];
+    document.getElementById('channel_list').appendChild(a);
+    document.getElementById('channel_list').appendChild(br);
+  };
+});
