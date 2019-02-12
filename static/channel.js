c194d7c9fe43faf4803e471dad152ed3ddb20979
frealis <36343653+frealis@users.noreply.github.com>
2019-02-12 11:15:04 -0500

    Removed the code that prevents space keystrokes from channel.js and channel.html because it would be weird to have a chatroom without spaces

--- static/channel.js
+++ static/channel.js
@@ -45,9 +45,3 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('#chatroom').append(br);
   });
 });
-
-// https://jsfiddle.net/taditdash/hDtA3/
-function AvoidSpace(event) {
-  var k = event ? event.which : window.event.keyCode;
-  if (k == 32) return false;
-};
