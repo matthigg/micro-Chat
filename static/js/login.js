fdff36592c43e1e17e7b80b36f2589e2e4bc1746
frealis <36343653+frealis@users.noreply.github.com>
2019-05-04 16:55:53 -0400

    Add main.scss to ./static/css in order to start using Sass, update login.html UI

--- static/js/login.js
+++ static/js/login.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  // By default, submit button is disabled
-  document.querySelector('#submit').disabled = true;
-
-  // Enable button only if there is text in the input field
+  // Enable submit button only if there is text in the input field
   document.querySelector('#username').oninput = () => {
     if (document.querySelector('#username').value.length > 0)
-      document.querySelector('#submit').disabled = false;
+      document.querySelector('.submit-username').disabled = false;
     else
-      document.querySelector('#submit').disabled = true;
+      document.querySelector('.submit-username').disabled = true;
   };
 });
 
